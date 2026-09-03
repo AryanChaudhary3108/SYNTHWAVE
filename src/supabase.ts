6b79866cdc52df349059d90e8a9560444efa866e
Aryan Chaudhary <aryanchaudhary44698@gmail.com>
2026-09-03 17:48:35 +0530

    chore: update supabase credentials

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vintbyyudkwrzuxhgmim.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpbnRieXl1ZGt3cnp1eGhnbWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5MDAyMDAsImV4cCI6MjA0ODQ3NjIwMH0.F_n2kh7jFkGHssmeAGtr6NJBXKsgSUKCzL1Trln0JFQ";
+const supabaseUrl = "https://ozeppmmugeijbejphiub.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96ZXBwbW11Z2VpamJlanBoaXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MzEwNTgsImV4cCI6MjEwNDAwNzA1OH0._UKGzqI132r1ks_FnDVsOQg9_t9f83kn0hI6zxtxExg";
 export const supabase = createClient(supabaseUrl, supabaseKey);
